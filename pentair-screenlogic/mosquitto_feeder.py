9af90ee06f9436255e8249361572928a430ec9eb
Scott Presnell <srp@tworoads.net>
2020-08-31 18:25:20 -0700

    Added client id

--- pentair-screenlogic/mosquitto_feeder.py
+++ pentair-screenlogic/mosquitto_feeder.py
@@ -1,11 +1,17 @@
 import paho.mqtt.client as mqtt
+import platform
 import time
 import sys
 import re
+import os
 
 
 def main(argv):
-  client = mqtt.Client()
+  my_host = platform.node()
+  my_pid = os.getpid()
+  client_id = my_host + '-' + str(my_pid)
+
+  client = mqtt.Client(client_id)
   client.username_pw_set(username=argv[3], password=argv[4])
   client.connect(argv[1], int(argv[2]) , 60)
   client.loop_start()
