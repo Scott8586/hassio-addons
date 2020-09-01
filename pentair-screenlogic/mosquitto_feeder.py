fd7ab57ac0d6f3c8d3ffcab38b3d8723163a2de8
Scott Presnell <srp@tworoads.net>
2020-08-31 20:28:08 -0700

    Added some error checking to loops

--- pentair-screenlogic/mosquitto_feeder.py
+++ pentair-screenlogic/mosquitto_feeder.py
@@ -5,18 +5,32 @@ import sys
 import re
 import os
 
+def on_connect(client, userdata, flags, return_code):
+    """function to mark the connection to a MQTT server
+    """
+
+    if return_code != 0:
+        print("Connected with result code: ", str(return_code))
+    else:
+        client.connected_flag=True
+
 
 def main(argv):
   my_host = platform.node()
   my_pid = os.getpid()
   client_id = my_host + '-' + str(my_pid)
 
+  mqtt.Client.connected_flag=False #create flag in class
   client = mqtt.Client(client_id)
+  client.on_connect=on_connect  #bind call back function
   client.username_pw_set(username=argv[3], password=argv[4])
   client.connect(argv[1], int(argv[2]) , 60)
   client.loop_start()
+  while not client.connected_flag: #wait in loop
+    time.sleep(1)
 
   for line in sys.stdin:
+    line.rstrip()
     m = re.match('(.*),(.*)', line)
     if m:
       client.publish(m.group(1), payload=m.group(2))
